a5705954959414d8be37c5e1417c1ce2857c4654
sfrjrs <json.spence@gmail.com>
2016-12-20 20:30:42 -0800

    add 'script' gulp task to minify main,js file

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cssmin = require('gulp-cssmin');
+var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var shell = require('gulp-shell');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+
+//script paths
+var jsFiles = 'js/main.js',
+    jsDest = 'js/min';
+
+gulp.task('scripts', function() {
+  return gulp.src(jsFiles)
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(jsDest))
+    .pipe(rename('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDest));
+});
 
 // Compile sass files
 gulp.task('sass', function() {
---
+++ js/min/main.js
@@ -0,0 +1,58 @@
+
+$(function() {
+
+	// Cache selectors
+	var contactForm = $('#contactForm'),
+			sectionForm = $('.section-form'),
+			sectionFormConfirm = $('.section-confirm'),
+			anchorScrollEle = $('a.page-scroll'),
+			firstName = $('#FirstName'),
+			lastName = $('#LastName'),
+			emailAddr = $('#EmailAddress'),
+			inquiry = $('#Inquiry'),
+			message = $('#Message');
+
+	// Initialize form validation
+	$(contactForm).parsley();
+
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+  $(anchorScrollEle).bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+  });
+
+	$(contactForm).submit(function (e) {
+		e.preventDefault();
+
+		var formData = {
+				'First Name': 	$(firstName).val(),
+				'Last Name': 	$(lastName).val(),
+				'Email Address': 	$(emailAddr).val(),
+				'Inquiry': $(inquiry).val(),
+				'Message': $(message).val()
+		};
+
+		$.ajax({
+		    url: 'https://formspree.io/info@daeda.io',
+		    method: 'POST',
+		    data: formData,
+		    dataType: 'json'
+		})
+		.done(function(data) {
+
+			$(sectionForm).hide('400', function(){
+				$(sectionFormConfirm).show();
+				$('html, body').stop.animate({
+					scrollTop: $(contactForm).offset().top
+				});
+			});
+			$(contactForm)[0].reset();
+
+		});
+
+	});
+
+});
